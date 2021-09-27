c38ca4cbd8a01e1ea3fa9ca4ae79298eb3697b08
balvindersingh <balvindersingharora7@gmail.com>
2021-09-27 19:54:42 +0530

    some changes in ui

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,14 +1,33 @@
 import { Link } from "react-router-dom"
+import '../NavBar/navbar.scss'
 
-const Navbar =()=>
-{
-   return(
-       <>
-       <Link to="/">Home</Link>
-       <Link to="/product-list">Product List</Link>
-       <Link to="/login">Login</Link>
-       <Link to="/add_card">Add card</Link>
-       </>
-   )
+const Navbar = () => {
+    const collapse = () => {
+        document.getElementById('collapse').classList.toggle('mobile-nav');
+        document.getElementById('collapse').classList.toggle('is-active');
+    }
+    return (
+        <>
+            
+               
+                    <nav class="navbar">
+                        <h3 className="navbar-brand">Shoppify </h3>
+                        <div class="menu-toggle" id="mobile-menu">
+                        <i class="fas fa-bars" onClick={collapse}/>
+                        </div>
+                        <ul class="nav no-search" id="collapse">
+                            <li class="nav-item"><Link className="navbar-linkes" to='/'>Home</Link></li>
+                            <li class="nav-item"><Link className="navbar-linkes" to='/products'>Products</Link></li>
+                            <li class="nav-item"><Link className="navbar-linkes" to='/'>Home</Link></li>
+                            <li class="nav-item"><Link className="navbar-linkes" to='/'>Home</Link></li>
+                            <li class="nav-item"><Link className="navbar-linkes" to='/login'>Login</Link></li>
+
+                        </ul>
+                    </nav>
+                
+
+          
+        </>
+    )
 }
 export default Navbar
